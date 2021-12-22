353dab7353bfd494eccd7cba30f2e74d6167d9fa
Mr-Destructive <gormeet711@gmail.com>
2021-12-22 21:42:44 +0530

    added update view from template

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, Update_List
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="homepage"),
+    path('update/', Update_List.as_view(), name="update_homepage"),
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView
+from django.shortcuts import render
+from django.core.management import call_command
 
 from .models import Episode
 
@@ -10,3 +12,15 @@ class HomePageView(ListView):
         context = super().get_context_data(**kwargs)
         context["episodes"] = Episode.objects.filter().order_by("-pub_date")[:12]
         return context
+
+class Update_List(ListView):
+    model = Episode
+    template_name = "homepage.html"
+    
+    def get_context_data(self, **kwargs):
+        if ( self.request.method == "GET" ):
+            call_command('startjobs')
+        context = super().get_context_data(**kwargs)
+        context["episodes"] = Episode.objects.filter().order_by("-pub_date")[:12]
+        return context
+
